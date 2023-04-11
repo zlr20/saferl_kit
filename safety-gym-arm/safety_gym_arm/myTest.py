5485986bedb10331679be1b1f90efd8f121ebfaa
YIFANSUN98 <910720891syf@gmail.com>
2023-04-10 20:04:34 -0400

    remove robot keepout range

--- safety-gym-arm/safety_gym_arm/myTest.py
+++ safety-gym-arm/safety_gym_arm/myTest.py
@@ -41,7 +41,7 @@ def run_random(env_name):
         'render_lidar_radius': 0.25,
         'hazard3Ds_num': 0,
         # 'hazard3Ds_locations':[(0.0,1.5)],
-        'hazards_num': 0,
+        'hazards_num': 1,
         'vases_num': 0,
         # 'vases_size': 0.2,
         # 'robot_locations':[(0.0,0.0)],
@@ -73,45 +73,58 @@ def run_random(env_name):
     }
     
     config = {
-            'robot_base': 'xmls/point.xml',
-            'robot_keepout': 0.3,
-            'robot_keepout_range': 0.1,
-            'num_steps': 100,
-            # 'robot_locations':[(0.0,0.0)],
-            'goal_3D': True,
-            'robot_keepout_range':0.1,
-            'observe_goal_comp': True,
-            'compass_shape':3,
-            'task': 'goal',
-            'observe_ghosts': True,
-            'observe_ghosts': True,
-            'observation_flatten': True,
-            'lidar_max_dist': 4,
-            'lidar_num_bins': 10,
-            'lidar_num_bins3D': 6,
-            'render_lidar_radius': 0.25,
-            'constrain_indicator':False,
-            'hazards_num': 1,
-            'hazard3Ds_size': 0.4,
-            'observe_hazard3Ds': True,
-            'constrain_hazards': True,
-            'constrain_ghosts': True,
-            'ghosts_num': 0,
-            'ghosts_size': 0.3,
-            'ghosts_mode': 'catch',
-            'ghosts_travel':2.5,
-            'ghosts_velocity': 0.001,
-            'ghosts_contact':False,
+        'robot_base': 'xmls/point.xml', # dt in xml, default 0.002s for point
+        'task': 'goal',
+        'observation_flatten': True,  # Flatten observation into a vector
+        'observe_sensors': True,  # Observe all sensor data from simulator
+        # Sensor observations
+        # Specify which sensors to add to observation space
+        'sensors_obs': ['accelerometer', 'velocimeter', 'gyro', 'magnetometer'],
+        'sensors_hinge_joints': True,  # Observe named joint position / velocity sensors
+        'sensors_ball_joints': True,  # Observe named balljoint position / velocity sensors
+        'sensors_angle_components': True,  # Observe sin/cos theta instead of theta
+        'continue_goal': False, # Done when goal is reached 
 
-            'constrain_ghost3Ds': False,
-            'ghost3Ds_num': 0,
-            'ghost3Ds_size': 0.2,
-            'ghost3Ds_travel':2.0,
-            'ghost3Ds_mode': 'catch',
-            'ghost3Ds_velocity': 0.001,
-            'ghost3Ds_z_range': [0.1,0.1],
-            'ghost3Ds_contact':False,
-        }
+        #observe goal/box/...
+        'observe_goal_dist': False,  # Observe the distance to the goal
+        'observe_goal_comp': False,  # Observe a compass vector to the goal
+        'observe_goal_lidar': True,  # Observe the goal with a lidar sensor
+        'observe_box_comp': False,  # Observe the box with a compass
+        'observe_box_lidar': False,  # Observe the box with a lidar
+        'observe_circle': False,  # Observe the origin with a lidar
+        'observe_remaining': False,  # Observe the fraction of steps remaining
+        'observe_walls': False,  # Observe the walls with a lidar space
+        'observe_hazards': False,  # Observe the vector from agent to hazards
+        'observe_vases': False,  # Observe the vector from agent to vases
+        'observe_pillars': True,  # Lidar observation of pillar object positions
+        'observe_buttons': False,  # Lidar observation of button object positions
+        'observe_gremlins': False,  # Gremlins are observed with lidar-like space
+        'observe_vision': False,  # Observe vision from the robot
+
+        # Constraints - flags which can be turned on
+        # By default, no constraints are enabled, and all costs are indicator functions.
+        'constrain_hazards': False,  # Constrain robot from being in hazardous areas
+        'constrain_vases': False,  # Constrain frobot from touching objects
+        'constrain_pillars': True,  # Immovable obstacles in the environment
+        'constrain_buttons': False,  # Penalize pressing incorrect buttons
+        'constrain_gremlins': False,  # Moving objects that must be avoided
+        # cost discrete/continuous. As for AdamBA, I guess continuous cost is more suitable.
+        'constrain_indicator': False,  # If true, all costs are either 1 or 0 for a given step. If false, then we get dense cost.
+
+        #lidar setting
+        'lidar_max_dist': None, # Maximum distance for lidar sensitivity (if None, exponential distance)
+        'lidar_num_bins': 16,
+        #num setting
+        'pillars_num': 8,
+        # 'pillars_size': 0.3,
+
+        # Frameskip is the number of physics simulation steps per environment step
+        # Frameskip is sampled as a binomial distribution
+        # For deterministic steps, set frameskip_binom_p = 1.0 (always take max frameskip)
+        'frameskip_binom_n': 10,  # Number of draws trials in binomial distribution (max frameskip) 
+        'frameskip_binom_p': 1.0  # Probability of trial return (controls distribution)
+    }
+
         
     env = Engine(config)
     obs = env.reset()
@@ -132,7 +145,7 @@ def run_random(env_name):
             obs = env.reset()
         assert env.observation_space.contains(obs)
         act = env.action_space.sample()
-        act = np.zeros(act.shape)
+        # act = np.zeros(act.shape)
         # print(act)
         # cnt = cnt + 1
         # if cnt % 400 > 200:
